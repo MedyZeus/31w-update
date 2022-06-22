3ba78a9f8b4378cb92909ccab4834c1aa9172d57
Mahdi Nadir <mehdinip@gmail.com>
2022-06-21 21:31:52 -0400

    afficher les posts a index.php et ajouter et modifier le fichier style.scss

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php get_header(); ?>
 <h1>Mon premier modèle WordPress</h1>
+<?php
+    if (have_posts()) 
+        while (get_post()){ the_post();?>
+        <p><?php
+            the_title();
+        }
+        ?></p>
+<?php //endwhile; ?>
+<?php //endif; ?>
 <?php get_footer(); ?>
