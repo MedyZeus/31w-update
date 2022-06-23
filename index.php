27bce4cd5aa1a4e672386c7203ea0fecfa20c9a7
Mahdi Nadir <mehdinip@gmail.com>
2022-06-22 20:09:49 -0400

    modifier la structure de index.php et ajouter filetime() dans ex1_31w.php

--- index.php
+++ index.php
@@ -3,10 +3,15 @@
 <?php
     if (have_posts()) :
         while (have_posts()) : the_post();?>
-        <p><?php
-            the_title();
-        
-        ?></p>
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_content(); ?></p>
+        <?php
+            /* $contenu = get_the_content();
+            echo wp_trim_words($contenu, 20, "..."); */
+        ?>
+        <?php $lien = "" ?>
+        <p><?= wp_trim_words(get_the_content(), 20, ) ?></p>
+
 <?php endwhile; ?>
 <?php endif; ?>
 
